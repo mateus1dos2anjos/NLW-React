d32db4f7f8616d46f49bb48f1778f9c939de9551
mateus1dos2anjos <mateus.16745@gmail.com>
2021-04-19 18:46:57 -0300

    Aula 1

---
+++ podcastr/src/Button.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function Button(props) {
+    const [counter, setCounter] = useState(1);
+
+    // [estado, alterarEstado]
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <>
+            <span>{counter}</span>
+            <button onClick={increment}> {props.children} </button>
+            <br />
+        </>
+    )
+}
